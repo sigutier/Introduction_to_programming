92cead88e10f471fa93ea6becd938963f5d5e9c1
sigutier <slvgutierrezrodriguez@gmail.com>
2022-09-11 14:29:30 +0200

    exercises

---
+++ 01 - Fundamentos de Programación I/02-fortalezadeunacontraseña.py
@@ -0,0 +1,14 @@
+# Fortaleza de una contraseña
+
+""" Crea un programa que dtermine la complejidad de una contraseña en base a las siguientes reglas:
+
+1. Una contraseña muy débil contiene sólo números y su longitud es menor de 8 carácteres
+2. Una contraseña  débil contiene sólo letras y su longitud es menor de 8 carácteres
+3. Una contraseña es fuerte si contiene letras y al menos un número y tiene al menos 8 carácteres de longitud
+4. Una contraseña es muy fuerte si tiene letras, números y caracteres especiales con al menos 8 carácteres de longitud
+
+Restricciones:
+1. Crear una función `validaPwd` que devuelva un valor que evaluado por otro programa determine si la contraseña es 
+    muy débil, débil, fuerte o muy fuerte. No devuelvas una cadena, debe servir para otros lenguajes.
+2. Utilizar una sentencia de salida única """
+
