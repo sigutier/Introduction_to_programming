2013449325c4a3ebd058560f1be033a36c7aa406
sigutier <slvgutierrezrodriguez@gmail.com>
2022-09-13 19:50:49 +0200

    exercises

--- 01 - Fundamentos de Programación I/02-fortalezadeunacontraseña.py
+++ 01 - Fundamentos de Programación I/02-fortalezadeunacontraseña.py
@@ -1,6 +1,6 @@
 # Fortaleza de una contraseña
 
-""" Crea un programa que dtermine la complejidad de una contraseña en base a las siguientes reglas:
+""" Crea un programa que determine la complejidad de una contraseña en base a las siguientes reglas:
 
 1. Una contraseña muy débil contiene sólo números y su longitud es menor de 8 carácteres
 2. Una contraseña  débil contiene sólo letras y su longitud es menor de 8 carácteres
@@ -12,3 +12,51 @@ Restricciones:
     muy débil, débil, fuerte o muy fuerte. No devuelvas una cadena, debe servir para otros lenguajes.
 2. Utilizar una sentencia de salida única """
 
+import re
+
+# muy debil
+def es_muy_debil(password):
+    pattern = re.compile('^[0-9]{0,8}$')
+    if pattern.match(password) != None:
+        return True
+    else: 
+        return False
+
+# debil
+def es_debil(password):
+    pattern = re.compile('^[a-zA-Z]{0,8}$')
+    if pattern.match(password) != None:
+        return True
+    else: 
+        return False
+
+# fuerte
+def es_fuerte(password):
+    pattern = re.compile("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+    if pattern.match(password) != None:
+        return True
+    else: 
+        return False
+
+# muy fuerte
+def es_muy_fuerte(password):
+    pattern = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$")
+    if pattern.match(password) != None:
+        return True
+    else: 
+        return False
+
+
+def validaPwd(password):
+    if es_muy_debil(password):
+        print("Contraseña muy debil")
+    elif es_debil(password):
+        print("Contraseña debil")
+    elif es_fuerte(password):
+        print("Contraseña fuerte")
+    elif es_muy_fuerte(password):
+        print("Contraseña muy fuerte")
+
+
+input_password = input("Enter your password: ")
+validaPwd(input_password)
